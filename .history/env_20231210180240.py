00c28d3d6f640d08e04fbb911022b9b7a156eeeb
JaySanders95 <jaysanders1878@gmail.com>
2023-12-10 18:07:04 +0000

    initial commit and database setup

---
+++ .history/env_20231210180240.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ["DATABASE_URL"]
---
+++ .history/env_20231210180333.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"]="postgres://mmrgggcp:YwegitAenbm0Rk7xNc0V23szcFFBK6YM@flora.db.elephantsql.com/mmrgggcp"
+
+os.environ["SECRET_KEY"]="SUPER-SECRET-KEY"
