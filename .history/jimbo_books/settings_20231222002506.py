8e076433ee721777a3608b3eff23ae319520d70e
JaySanders95 <jaysanders1878@gmail.com>
2023-12-22 16:24:29 +0000

    added fontawesome + buttons to bag for checkout and keep shopping

--- .history/jimbo_books/settings_20231222002506.py
+++ .history/jimbo_books/settings_20231222002506.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-jaysanders9-jimbosbooks-w3o1iwkoq6t.ws-eu107.gitpod.io', 'jimbo-books.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-jaysanders9-jimbosbooks-w3o1iwkoq6t.ws-eu107.gitpod.io/', 'jimbo-books.herokuapp.com', 'localhost']
 
 
 # Application definition
