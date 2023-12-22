6dbf7123bf53bb788e19ecd3c1c3bd7566370d49
JaySanders95 <jaysanders1878@gmail.com>
2023-12-22 19:03:19 +0000

    added messages functionality

---
+++ .history/bag/contexts_20231222180310.py
@@ -0,0 +1,48 @@
+from decimal import Decimal
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from home.models import Book 
+
+
+def remove_from_bag(request, item_id):
+    bag = request.session.get('bag', {})
+
+    if item_id in abg
+
+
+def bag_contents(request):
+
+    bag_items = []
+    total = 0 
+    book_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        book = get_object_or_404(Book, pk=item_id)
+        total += quantity * book.price
+        book_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity' : quantity,
+            'book' : book,
+
+        })
+
+    if bag_items:
+        delivery = int(4)
+    else:
+        delivery = 0
+
+
+
+    grand_total = delivery + total
+
+    context = {
+        'bag_items' : bag_items,
+        'total' : total,
+        'book_count' : book_count,
+        'delivery' : delivery,
+        'grand_total' : grand_total
+    }
+
+    return context
