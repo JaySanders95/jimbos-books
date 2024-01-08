9000eedb1c3c230b001809e0021b5b4e0d405ab6
JaySanders95 <jaysanders1878@gmail.com>
2024-01-08 14:54:32 +0000

    added form validity for checkout

--- .history/checkout/forms_20240108143914.py
+++ .history/checkout/forms_20240108143914.py
@@ -55,3 +55,27 @@ class OrderForm(forms.ModelForm):
             Field('street_address2', css_class='stripe-style-input'),
             Field('county', css_class='stripe-style-input'),
         )
+    def clean_phone_number(self):
+        return self.clean_generic_field('phone_number', r'^[0-9+]*$', "Phone number can only contain digits and '+' sign.")
+
+    def clean_postcode(self):
+        return self.clean_generic_field('postcode', r'^[0-9A-Za-z\s]*$', "Postcode can only contain alphanumeric characters and spaces.")
+
+    def clean_town_or_city(self):
+        return self.clean_generic_field('town_or_city', r'^[0-9A-Za-z\s]*$', "Town or city can only contain alphanumeric characters and spaces.")
+
+    def clean_street_address1(self):
+        return self.clean_generic_field('street_address1', r'^[0-9A-Za-z\s]*$', "Street Address 1 can only contain alphanumeric characters and spaces.")
+
+    def clean_street_address2(self):
+        return self.clean_generic_field('street_address2', r'^[0-9A-Za-z\s]*$', "Street Address 2 can only contain alphanumeric characters and spaces.")
+
+    def clean_county(self):
+        return self.clean_generic_field('county', r'^[0-9A-Za-z\s]*$', "County can only contain alphanumeric characters and spaces.")
+
+    def clean_generic_field(self, field_name, regex_pattern, error_message):
+        field_value = self.cleaned_data.get(field_name)
+        # Check if the field contains special characters
+        if not re.match(regex_pattern, field_value):
+            raise ValidationError(error_message)
+        return field_value
