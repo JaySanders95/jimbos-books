81ad5fe2ca9be60d1f785bb3c83de86683f01f03
JaySanders95 <jaysanders1878@gmail.com>
2024-01-09 17:27:35 +0000

    email adfd

--- .history/jimbo_books/settings_20240109172626.py
+++ .history/jimbo_books/settings_20240109172626.py
@@ -208,7 +208,7 @@ if 'DEVELOPMENT' in os.environ:
     DEFAULT_FROM_EMAIL = 'jimbosbooks019@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = False
+    EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
